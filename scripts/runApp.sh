1489a62d750b079fee6c8927d92a265e28472241
Chethan123 <32221994+ChethanNag@users.noreply.github.com>
2021-03-11 20:09:17 +0530

    Update runApp.sh

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -4,11 +4,11 @@
 
 echo 'The following "npm" command runs your Node.js application'
 
-set -x
+
 npm start &
 
 
-set +x
+
 
 echo 'Now you can'
 echo 'Visit http://ServerIp:9981 to see your Node.js application'
